da273fd99658329186c5e2e5a3c949925927203e
dly99j <dly99j@inf.elte.hu>
2022-03-02 15:07:45 +0100

    finished 0th task

--- id_bimap.hpp
+++ id_bimap.hpp
@@ -6,12 +6,41 @@
 #define HALADO_CPP_BEAD_ID_BIMAP_HPP
 
 #include <type_traits>
+#include "avl_tree.hpp"
 
-//TODO check if integral type and if comparable, exception if not
+//TODO check if integral type and if comparable, exception if not (this is ugly! - i mean the exception, the solution is nice)
 //I might need my own exceptions
-template<typename Key, typename Value>
+template<   typename Mapped,
+            typename Key = std::size_t ,
+            typename = typename std::enable_if_t< std::is_integral_v<Key> >,
+            typename = typename std::enable_if_t< !std::is_same< Mapped, Key >::value > >
 class id_bimap {
+public:
 
+    using mapped_type = Mapped;
+    using key_type = Key;
+
+    id_bimap() {
+
+    }
+
+    explicit id_bimap(const id_bimap& other) {
+        this->key_to_data = other.key_to_data;
+        this->data_to_key = other.data_to_key;
+    }
+
+    ~id_bimap() {
+
+    }
+
+private:
+    avl_tree<Key, Mapped> key_to_data;
+    avl_tree<Mapped, Key> data_to_key;
 };
+
+template<typename T>
+using kchar_id_bimap = id_bimap<T, char>;
+
+using string_id_bimap = id_bimap<std::string>;
 
 #endif//HALADO_CPP_BEAD_ID_BIMAP_HPP
--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
-#include "avl_tree.hpp"
+#include <string>
+#include "id_bimap.hpp"
+
+#define NFAIL
+
+void F0_types() {
+#ifndef NFAIL
+    // EXPECT FAIL: Template parameter "value" must always be specified.
+    id_bimap<> A;
+
+    // EXPECT FAIL: Template parameter "value" must always be specified.
+    kchar_id_bimap<> B;
+
+    // EXPECT FAIL: Key must be integer!
+    id_bimap<std::string, float> F;
+
+    // EXPECT FAIL: Key and value must be separate types.
+    id_bimap<int, int> I;
+    id_bimap<std::string, std::string> S;
+    id_bimap<const std::string, std::string> S2;
+#endif
+
+    string_id_bimap SM;
+    static_assert(std::is_same_v<decltype(SM)::mapped_type, std::string>);
+
+    struct T {};
+    id_bimap<T> TM;
+    static_assert(sizeof(decltype(TM)::key_type) == sizeof(&TM));
+    static_assert(std::is_same_v<decltype(TM)::mapped_type, T>);
+
+    id_bimap<T, short> STM;
+    static_assert(std::is_same_v<decltype(STM)::key_type, short>);
+    static_assert(std::is_same_v<decltype(STM)::mapped_type, T>);
+
+    kchar_id_bimap<T> CTM;
+    static_assert(std::is_same_v<decltype(CTM)::key_type, char>);
+    static_assert(std::is_same_v<decltype(CTM)::mapped_type, T>);
+}
 
 int main() {
-    avl_tree<int, std::string> tree;
-    tree.insert(5, "asd");
-    tree.insert(6, "sdfs");
-    tree.insert(8, "sdfsddfs");
-
-    tree.erase(6);
-    auto var = tree.find(5);
-
-    //this is some god damn ugly solution
-    //still better than returning a null pointer i guess
-    std::cout << var.value_or(std::make_shared< avl_node <int, std::string > >(-1, std::string("error")))->data;
-    return 0;
+    F0_types();
 }
