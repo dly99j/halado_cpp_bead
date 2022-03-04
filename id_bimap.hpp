5b7ea891cfc62f0e8f3e8093172f56e4808b5028
dly99j <dly99j@inf.elte.hu>
2022-03-04 01:42:00 +0100

    first commit for 1st task

--- id_bimap.hpp
+++ id_bimap.hpp
@@ -10,15 +10,27 @@
 
 //TODO check if integral type and if comparable, exception if not (this is ugly! - i mean the exception, the solution is nice)
 //I might need my own exceptions
-template<   typename Mapped,
-            typename Key = std::size_t ,
-            typename = typename std::enable_if_t< std::is_integral_v<Key> >,
-            typename = typename std::enable_if_t< !std::is_same< Mapped, Key >::value > >
+template<   class Mapped,
+            class Key = std::size_t,
+            class Compare = std::less<Key>,
+            class Allocator = std::allocator<std::pair<const Key, Mapped> >,
+            class = typename std::enable_if_t< std::is_integral_v<Key> >,
+            class = typename std::enable_if_t< !std::is_same< Mapped, Key >::value > >
 class id_bimap {
 public:
 
     using mapped_type = Mapped;
     using key_type = Key;
+    using value_type = std::pair<Key, Mapped>;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t; //questionmark? TODO look this up
+    using key_compare = Compare;
+    using allocator_type = Allocator;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = typename std::allocator_traits<Allocator>::pointer;
+    using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
+    //TODO other member types
 
     id_bimap() {
 
@@ -34,8 +46,9 @@ public:
     }
 
 private:
-    avl_tree<Key, Mapped> key_to_data;
-    avl_tree<Mapped, Key> data_to_key;
+    std::size_t m_size;
+    avl_tree<Key, Mapped> m_key_to_data;
+    avl_tree<Mapped, Key> m_data_to_key;
 };
 
 template<typename T>
