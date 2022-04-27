236269dd111f95a4cecb97a95fef4d8d7cceba0c
htomi <dly99j@inf.elte.hu>
2022-04-27 11:52:33 +0200

    aaa

--- id_bimap.hpp
+++ id_bimap.hpp
@@ -7,12 +7,14 @@
 
 #include <type_traits>
 #include <memory>
-#include <map>
+#include <unordered_map>
 #include <stdexcept>
 
 //TODO check if integral type and if comparable, exception if not (this is ugly! - i mean the exception, the solution is nice)
 //I might need my own exceptions
 namespace bead {
+
+    //constexpr auto variable = [](const auto& val) { return val; };
 
     template<class Mapped,
              class Key = std::size_t,
@@ -36,31 +38,39 @@ namespace bead {
 
     public:
 //iterator
-        using iterator = typename std::map<Mapped, Key, Compare, Allocator>::iterator;
-
+        using iterator          = typename std::unordered_map<Mapped, Key, Compare, Allocator>::iterator;
+        using const_iterator    = typename std::unordered_map<Mapped, Key, Compare, Allocator>::const_iterator;
 //constructors/destructor
     public:
         id_bimap();
         id_bimap(const id_bimap &other);//TODO move constructor
+        id_bimap(std::initializer_list<Mapped>);
         ~id_bimap();
 
 //public member functions
     public:
-        iterator begin();
-        iterator end();
-        std::pair<iterator, bool> insert(const Mapped &value); //TODO other signatures
+        const_iterator begin();
+        const_iterator end();
+        std::pair<const_iterator, bool> insert(const Mapped &value); //TODO other signatures
 
         const Mapped& operator[](const Key& k);
         const Key& operator[](const Mapped& m);
 
-        bool empty();
-        size_type size();
-        void clear();
+        auto empty();
+        auto size();
+        auto clear();
+        auto erase(const Key& key);
+        auto erase(const Mapped& mapped);
+        auto find(const Key& key);
+        auto find(const Mapped& mapped);
 
 //private functions
     private:
+        auto find_iter(const Mapped& m);
+
+    private:
         std::size_t                                 m_size{};
-        std::map<Key, Mapped, Compare, Allocator>   m_key_to_data;
+        std::unordered_map<Key, Mapped, Compare, Allocator>   m_key_to_data;
         //std::map<Key, Mapped, Compare, Allocator>   m_data_to_key;
     };
 
@@ -84,8 +94,17 @@ bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap(const id_bimap &other)
     map_constructor_check<Mapped, Key>();
     this->m_size = other.m_size;
     this->m_key_to_data = other.m_key_to_data;
-    this->m_data_to_key = other.m_data_to_key;
+    //this->m_data_to_key = other.m_data_to_key;
 }
+
+template<class Mapped, class Key, class Compare, class Allocator>
+bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap(std::initializer_list<Mapped> l) {
+    map_constructor_check<Mapped, Key>();
+    for (auto i : l) {
+        this->insert(i);
+    }
+}
+
 
 template<class Mapped, class Key, class Compare, class Allocator>
 bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap::~id_bimap() {
@@ -95,22 +114,22 @@ bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap::~id_bimap() {
 //map member function definitions
 
 template<class Mapped, class Key, class Compare, class Allocator>
-typename bead::id_bimap<Mapped, Key, Compare, Allocator>::iterator bead::id_bimap<Mapped, Key, Compare, Allocator>::begin() {
+typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterator bead::id_bimap<Mapped, Key, Compare, Allocator>::begin() {
     return m_key_to_data.begin();
 }
 
 template<class Mapped, class Key, class Compare, class Allocator>
-typename bead::id_bimap<Mapped, Key, Compare, Allocator>::iterator bead::id_bimap<Mapped, Key, Compare, Allocator>::end() {
+typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterator bead::id_bimap<Mapped, Key, Compare, Allocator>::end() {
     return m_key_to_data.end();
 }
 
 template<class Mapped, class Key, class Compare, class Allocator>
-std::pair<typename bead::id_bimap<Mapped, Key, Compare, Allocator>::iterator, bool>
+std::pair<typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterator, bool>
         bead::id_bimap<Mapped, Key, Compare, Allocator>::insert(const Mapped &value) {
 
     Key next_key;
 
-    if (m_key_to_data.size() == 0) {
+    if (m_key_to_data.empty()) {
         next_key = 0;
     } else {
         next_key = m_key_to_data.rbegin()->first + 1;
@@ -146,16 +165,49 @@ const Key& bead::id_bimap<Mapped, Key, Compare, Allocator>::operator[](const Map
     return to_return;
 }
 template<class Mapped, class Key, class Compare, class Allocator>
-bool bead::id_bimap<Mapped, Key, Compare, Allocator>::empty() {
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::empty() {
     return m_key_to_data.empty();
 }
 template<class Mapped, class Key, class Compare, class Allocator>
-typename bead::id_bimap<Mapped, Key, Compare, Allocator>::size_type bead::id_bimap<Mapped, Key, Compare, Allocator>::size() {
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::size() {
     return m_key_to_data.size();
 }
 template<class Mapped, class Key, class Compare, class Allocator>
-void bead::id_bimap<Mapped, Key, Compare, Allocator>::clear() {
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::clear() {
     m_key_to_data.clear();
+}
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::erase(const Key& key) {
+    m_key_to_data.erase(key);
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::erase(const Mapped& mapped) {
+    auto it = find_iter(mapped);
+    if (it == nullptr) return;
+    m_key_to_data.erase(it);
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find_iter(const Mapped& m) {
+    for (auto it = begin(); it != end(); ++it) {
+        if (it->first == m)
+            return it;
+    }
+    return nullptr;
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find(const Key& key) {
+    try {
+        return m_key_to_data.find(key);
+    } catch(...) {}
+    return nullptr;
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find(const Mapped& mapped) {
+    return find_iter(mapped);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-#include "avl_tree.hpp"
+#include "id_bimap.hpp"
 
-#define NFAIL
+//#define NFAIL
 /*
 using namespace bead;
 
@@ -40,12 +40,129 @@ void F0_types() {
     static_assert(std::is_same_v<decltype(CTM)::mapped_type, T>);
 }
 */
+#include <cassert>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+using namespace bead;
+
+void F1_operations()
+{
+    string_id_bimap SM;
+
+    assert(SM.size() == 0 && SM.empty());
+
+    // Exceptions.
+    try
+    {
+        auto S1 = SM["gsd"];
+        assert(false && "Unreachable.");
+    } catch (const std::domain_error&) {}
+
+    try
+    {
+        auto S2 = SM[8];
+        assert(false && "Unreachable.");
+    } catch (const std::out_of_range&) {}
+
+    // Insert and query.
+    auto IR1 = SM.insert("gsd");
+    assert(IR1.second);
+    assert(IR1.first->first == 0 && IR1.first->second == "gsd");
+
+    auto S1 = SM[0];
+    assert(S1 == "gsd");
+    auto S2 = SM["gsd"];
+    assert(S2 == 0);
+
+    assert(SM.size() == 1 && !SM.empty());
+
+#ifndef NFAIL
+    // EXPECT FAIL: Do not allow update through operator[].
+    SM[0] = "not-gsd";
+#endif
+
+    // Support equality-based lookup, not identity-based.
+    std::string W{"Whisperity"};
+
+    auto IR2 = SM.insert(W);
+    assert(IR2.second && IR2.first->first == 1 && IR2.first->second == W &&
+           IR2.first->second == "Whisperity");
+    // Idempotence.
+    assert(SM[SM[W]] == W);
+    assert(SM[SM[1]] == 1);
+    try
+    {
+        SM[2];
+        assert(false && "Unreachable.");
+    } catch (const std::out_of_range&) {}
+
+    assert(SM.size() == 2);
+
+    // Non-unique insertion.
+    std::string G{"gsd"};
+    auto IR3 = SM.insert(G);
+    assert(IR3.second == false && IR3.first->first == 0 &&
+           IR3.first->second == "gsd");
+    assert(SM.size() == 2);
+
+    // Copy.
+    const string_id_bimap CSM = SM;
+    {
+        string_id_bimap SM2 = SM;
+        SM2.clear();
+        assert(SM2.size() == 0 && SM2.empty());
+    }
+
+    // Proper copy!
+    assert(CSM.size() == 2 && !CSM.empty() && CSM["gsd"] == 0 &&
+           CSM["Whisperity"] == 1);
+    try
+    {
+        CSM[CSM["Xazax"]];
+        assert(false && "Unreachable.");
+    } catch (const std::domain_error&) {}
+
+    // Find.
+    assert(CSM.find("Whisperity") != CSM.end());
+    assert(CSM.find("Xazax") == CSM.end());
+    assert(CSM.find("gsd") != CSM.end() && CSM.find("gsd")->second == "gsd");
+
+    // Foreach iterator.
+    unsigned short Idx = 0;
+    for (const auto& E : CSM)
+    {
+        if (Idx == 0 && E.first == Idx)
+        {
+            ++Idx;
+            assert(E.second == G);
+        }
+        else if (Idx == 1 && E.first == Idx)
+        {
+            ++Idx;
+            assert(E.second == W);
+        } else
+            assert(false && "Expected only 2 elements in the copy!");
+    }
+
+    // Erase.
+    SM.erase("gsd");
+    assert(SM.size() == 1 && SM[0] == "Whisperity");
+    SM.erase(0);
+    assert(SM.empty());
+    assert(CSM.size() == 2);
+
+    // Initialisation list.
+    string_id_bimap SMInit = {"gsd", "Whisperity", "Bjarne", "Herb"};
+    std::ostringstream OSS;
+    for (const auto& E : SMInit)
+        OSS << E.second << ", ";
+    assert(OSS.str() == "gsd, Whisperity, Bjarne, Herb, ");
+}
 int main() {
     //F0_types();
 
-    avl_tree<int, std::string> tree;
-    tree.insert(1, "asd");
-    tree.insert(    tree.insert(2, "ddd");
-                3, "hi");
-    std::cout << tree.find(1).has_value() << '\n';
+
 }
