aac2ca7650d34f38ebd2956b76a9a42ea89f6813
dly99j <dly99j@inf.elte.hu>
2022-05-31 19:27:02 +0200

    fourth done

--- id_bimap.hpp
+++ id_bimap.hpp
@@ -8,6 +8,7 @@
 #include "utility.hpp"
 #include <algorithm>
 #include <map>
+#include <vector>
 #include <memory>
 #include <stdexcept>
 #include <type_traits>
@@ -65,7 +66,8 @@ namespace bead {
 
         auto empty() const noexcept;
         auto size() const noexcept;
-        auto clear();
+        auto capacity() const noexcept;
+        auto clear() noexcept;
         auto erase(const Key&);
         auto erase(const Mapped&);
         auto find(const Key&) const noexcept;
@@ -74,16 +76,20 @@ namespace bead {
         auto find_if(pred) const;
         template<typename pred>
         auto delete_all(pred) -> void;
+        auto next_index() const;
+        auto is_contiguous() const noexcept;
+        auto reserve(std::size_t);
 
 //private functions
     private:
         auto find_iter(const Mapped& m) const noexcept;
         auto reorder_map();
+        auto get_last_used_key() const noexcept;
 
     private:
-        std::size_t                                 m_size{};
         std::map<Key, Mapped, Compare, Allocator>   m_key_to_data;
-        //std::map<Key, Mapped, Compare, Allocator>   m_data_to_key;
+        std::vector<Key>                            m_deleted_indices;
+        std::size_t                                 m_unconstructed_reserved{};
     };
 
 //type aliases
@@ -103,22 +109,25 @@ bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap() {
 
 template<class Mapped, class Key, class Compare, class Allocator>
 bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap(const id_bimap& other)
-    : m_key_to_data(other.m_key_to_data),
-      m_size(other.m_size)
-      { }
+        : m_key_to_data(other.m_key_to_data),
+          m_deleted_indices(other.m_deleted_indices),
+          m_unconstructed_reserved(other.m_unconstructed_reserved)
+        { }
 
 template<class Mapped, class Key, class Compare, class Allocator>
 bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap(id_bimap&& other) noexcept
-    : m_key_to_data(std::move(other.m_key_to_data)),
-      m_size(std::move(other.m_size))
-      { }
+        : m_key_to_data(other.m_key_to_data),
+          m_deleted_indices(other.m_deleted_indices),
+          m_unconstructed_reserved(other.m_unconstructed_reserved)
+        { }
 
 template<class Mapped, class Key, class Compare, class Allocator>
 typename bead::id_bimap<Mapped, Key, Compare, Allocator>&
         bead::id_bimap<Mapped, Key, Compare, Allocator>::operator=(const id_bimap& other) {
     if (this != other) {
         this->m_key_to_data = other.m_key_to_data;
-        this->m_size = other.m_size;
+        this->m_deleted_indices = other.m_deleted_indices;
+        this->m_unconstructed_reserved = other.m_unconstructed_reserved;
     }
     return *this;
 }
@@ -128,7 +137,8 @@ typename bead::id_bimap<Mapped, Key, Compare, Allocator>&
         bead::id_bimap<Mapped, Key, Compare, Allocator>::operator=(id_bimap&& other) noexcept {
     if (this != other) {
         this->m_key_to_data = std::move(other.m_key_to_data);
-        this->m_size = std::move(other.m_size);
+        this->m_deleted_indices = std::move(other.m_deleted_indices);
+        this->m_unconstructed_reserved = std::move(other.m_unconstructed_reserved);
     }
     return *this;
 }
@@ -143,19 +153,32 @@ bead::id_bimap<Mapped, Key, Compare, Allocator>::id_bimap(std::initializer_list<
 }
 
 //map member function definitions
-
 template<class Mapped, class Key, class Compare, class Allocator>
 typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterator
         bead::id_bimap<Mapped, Key, Compare, Allocator>::begin() const noexcept {
 
-    return m_key_to_data.begin();
+    for (auto it = m_key_to_data.begin(); it != m_key_to_data.end(); ++it) {
+        if (std::find(m_deleted_indices.begin(),
+                      m_deleted_indices.end(),
+                      it->first) == m_deleted_indices.end()) {
+            return it;
+        }
+    }
+    return m_key_to_data.end();
 }
 
 template<class Mapped, class Key, class Compare, class Allocator>
 typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterator
         bead::id_bimap<Mapped, Key, Compare, Allocator>::end() const noexcept {
 
-    return m_key_to_data.end();
+    for (auto it = m_key_to_data.rbegin(); it != m_key_to_data.rend(); ++it) {
+        if (std::find(m_deleted_indices.begin(),
+                      m_deleted_indices.end(),
+                      it->first) == m_deleted_indices.end()) {
+            return it.base();
+        }
+    }
+    return m_key_to_data.begin();
 }
 
 template<class Mapped, class Key, class Compare, class Allocator>
@@ -168,6 +191,18 @@ std::pair<typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterat
         if (it->second == value) {
             return { it, false };
         }
+    }
+
+    if (!m_deleted_indices.empty()) {
+        auto min = std::min_element(m_deleted_indices.begin(), m_deleted_indices.end());
+        auto inserted = m_key_to_data.insert( {*min, value} );
+        if (inserted.second) {
+            --m_unconstructed_reserved;
+        } else {
+            m_key_to_data[*min] = value;
+        }
+        m_deleted_indices.erase(min);
+        return { find_iter(value), true };
     }
 
     if (m_key_to_data.empty()) {
@@ -184,9 +219,16 @@ template<typename... Args>
 std::pair<typename bead::id_bimap<Mapped, Key, Compare, Allocator>::const_iterator, bool>
 bead::id_bimap<Mapped, Key, Compare, Allocator>::emplace(Args&&... args) {
 
-    Key next_key;
-
-    if (m_key_to_data.empty()) {
+    Key next_key{};
+    if (!m_deleted_indices.empty()) {
+        auto min = std::min_element(m_deleted_indices.begin(), m_deleted_indices.end());
+        next_key = *min;
+        auto erased_num = m_key_to_data.erase(next_key);
+        if (erased_num == 0) {
+            --m_unconstructed_reserved;
+        }
+        m_deleted_indices.erase(min);
+    } else if (m_key_to_data.empty()) {
         next_key = 0;
     } else {
         next_key = m_key_to_data.rbegin()->first + 1;
@@ -199,7 +241,9 @@ const Mapped& bead::id_bimap<Mapped, Key, Compare, Allocator>::operator[](const 
 
     auto value = m_key_to_data.find(k);
 
-    if ( value == m_key_to_data.end() ) {
+    if ( value == m_key_to_data.end() || std::find(m_deleted_indices.begin(),
+                                                   m_deleted_indices.end(),
+                                                   value->first) != m_deleted_indices.end() ) {
         throw std::out_of_range("index out of range");
     }
 
@@ -210,7 +254,9 @@ template<class Mapped, class Key, class Compare, class Allocator>
 const Key& bead::id_bimap<Mapped, Key, Compare, Allocator>::operator[](const Mapped& m) const {
 
     for (const auto&[key, mapped] : m_key_to_data) {
-        if (mapped == m) {
+        if ( mapped == m && std::find(m_deleted_indices.begin(),
+                                      m_deleted_indices.end(),
+                                      key) == m_deleted_indices.end() ) {
             return key;
         }
     }
@@ -222,30 +268,42 @@ auto bead::id_bimap<Mapped, Key, Compare, Allocator>::empty() const noexcept {
 }
 template<class Mapped, class Key, class Compare, class Allocator>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::size() const noexcept {
-    return m_key_to_data.size();
+    return m_key_to_data.size() - m_deleted_indices.size() + m_unconstructed_reserved;
 }
 template<class Mapped, class Key, class Compare, class Allocator>
-auto bead::id_bimap<Mapped, Key, Compare, Allocator>::clear() {
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::capacity() const noexcept {
+    return m_key_to_data.size() + m_unconstructed_reserved;
+}
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::clear() noexcept {
     m_key_to_data.clear();
+    m_deleted_indices.clear();
 }
 template<class Mapped, class Key, class Compare, class Allocator>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::erase(const Key& key) {
-    m_key_to_data.erase(key);
-    reorder_map();
+    //m_key_to_data.erase(key);
+    m_key_to_data[key].Mapped::~Mapped();
+    m_deleted_indices.push_back(key);
+    //reorder_map();
 }
 
 template<class Mapped, class Key, class Compare, class Allocator>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::erase(const Mapped& mapped) {
     auto it = find_iter(mapped);
     if (it == end()) return;
+    auto key = it->first;
     m_key_to_data.erase(it);
-    reorder_map();
+    m_key_to_data[key].Mapped::~Mapped();
+    m_deleted_indices.push_back(key);
+    //reorder_map();
 }
 
 template<class Mapped, class Key, class Compare, class Allocator>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find_iter(const Mapped& m) const noexcept {
     for (auto it = begin(); it != end(); ++it) {
-        if (it->second == m)
+        if (it->second == m && std::find(m_deleted_indices.begin(),
+                                         m_deleted_indices.end(),
+                                         it->first) == m_deleted_indices.end())
             return it;
     }
     return end();
@@ -253,8 +311,14 @@ auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find_iter(const Mapped& m)
 
 template<class Mapped, class Key, class Compare, class Allocator>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find(const Key& key) const noexcept {
+    if ( std::find(m_deleted_indices.begin(),
+                   m_deleted_indices.end(),
+                   key) != m_deleted_indices.end()) {
+        return end();
+    }
     try {
-        Mapped m; //!!FONTOS!! nem szükséges, csak valamiért itt az assert vár plusz egy ctor-dtor hívást
+        Mapped m; //!!FONTOS!! nem szükséges, csak valamiért itt az assert vár plusz egy ctor-dtor
+                  //a második részfeladat
         return m_key_to_data.find(key);
     } catch(...) {}
     return end();
@@ -279,7 +343,9 @@ auto bead::id_bimap<Mapped, Key, Compare, Allocator>::reorder_map() {
 template<class Mapped, class Key, class Compare, class Allocator>
 template<typename pred>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::find_if(pred p) const {
-    auto npred = [&p](auto&& val){ return p(val.second); };
+    auto npred = [&p, this](auto&& val){ return p(val.second) && std::find(m_deleted_indices.begin(),
+                                                                           m_deleted_indices.end(),
+                                                                           val.first) == m_deleted_indices.end(); };
     return std::find_if(m_key_to_data.begin(), m_key_to_data.end(), npred);
 }
 
@@ -287,28 +353,83 @@ template<class Mapped, class Key, class Compare, class Allocator>
 template<typename pred>
 auto bead::id_bimap<Mapped, Key, Compare, Allocator>::delete_all(pred p) -> void {
 
-    auto npred = [&p](auto&& val){ return p(val.second); };
-    /*
-    for (auto it = m_key_to_data.begin();
-         it != m_key_to_data.end();
-         ++it) {
-
-        if (npred(*it)) {
-            m_key_to_data.erase(it);
-        }
-    }
-    reorder_map()
-    */
+    auto npred = [&p, this](auto&& val){ return p(val.second) &&
+                                                (std::find(m_deleted_indices.begin(),
+                                                           m_deleted_indices.end(),
+                                                           val.first) == m_deleted_indices.end()); };
     for (auto it = m_key_to_data.begin();
             it != m_key_to_data.end();
             ++it) {
 
         if (npred(*it)) {
             m_key_to_data.erase(it);
-            reorder_map();
             delete_all(p);
             break;
         }
+    }
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::next_index() const {
+    if (m_deleted_indices.empty()) {
+        Key next_key;
+        if (m_key_to_data.empty()) {
+            next_key = 0;
+        } else {
+            next_key = m_key_to_data.rbegin()->first + 1;
+        }
+        return next_key;
+    } else {
+        return *std::min_element(m_deleted_indices.begin(), m_deleted_indices.end());
+    }
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::is_contiguous() const noexcept {
+    return m_deleted_indices.size() == 0;
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::get_last_used_key() const noexcept {
+    if (m_key_to_data.empty()) {
+        return static_cast<std::size_t>(0);
+    } else {
+        return m_key_to_data.rbegin()->first;
+    }
+}
+
+template<class Mapped, class Key, class Compare, class Allocator>
+auto bead::id_bimap<Mapped, Key, Compare, Allocator>::reserve(std::size_t n_size) {
+    auto last_used_key = get_last_used_key();
+    std::size_t non_used{};
+    for (const auto& i : m_deleted_indices) {
+        if (i > last_used_key) ++non_used;
+    }
+    auto cap = capacity();
+    if (n_size < (cap - non_used)) return;
+    else if (n_size >= (cap - non_used) && n_size < cap) {
+        Key largest_key;
+        for (auto i = cap - 1; i > n_size - 1; --i) {
+            if (std::find(m_deleted_indices.begin(), m_deleted_indices.end(), i) != m_deleted_indices.end()) {
+                if (m_key_to_data.find(i) != m_key_to_data.end()) {
+                    m_key_to_data.erase(i);
+                } else {
+                    --m_unconstructed_reserved;
+                }
+                m_deleted_indices.erase(std::remove(m_deleted_indices.begin(),
+                                                    m_deleted_indices.end(),
+                                                    i), m_deleted_indices.end());
+            }
+        }
+        return;
+    }
+    else if (n_size > cap) {
+
+        for (auto i = cap; i < n_size; ++i) {
+            m_deleted_indices.push_back(i);
+            ++m_unconstructed_reserved;
+        }
+        return;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #define NFAIL
 
 using namespace bead;
-
+/*
 void F0_types() {
 #ifndef NFAIL
     // EXPECT FAIL: Template parameter "value" must always be specified.
@@ -169,7 +169,7 @@ void F1_operations()
 #include <stdexcept>
 #include <string>
 #include <utility>
-
+*/
 // IGNORE! Helper class for testing.
 struct SMFCounter
 {
@@ -212,7 +212,7 @@ std::size_t SMFCounter::Ctor, SMFCounter::CCtor, SMFCounter::MCtor,
 // IGNORE! Helper type: something that's nice enough not to be copyable,
 // but useful without a lot of hassle.
 using non_copyable = std::unique_ptr<std::string>;
-
+/*
 void F2_advanced()
 {
     SMFCounter::reset();
@@ -300,10 +300,159 @@ void F2_advanced()
            "Xazax, 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, "
            "32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, ");
 }
+*/
+#include <cassert>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
 
+void F3_logicalDelete()
+{
+    {
+        id_bimap<char, short> SCM;
+        static_assert(std::is_same_v<decltype(SCM.next_index()), short>,
+                      "Return index!");
+    }
+
+    string_id_bimap SM;
+    //std::cout << SM.size() << "  " << SM.capacity() << "  " << SM.next_index() << '\n';
+    assert(SM.size() == 0 && SM.capacity() == 0 &&
+           SM.next_index() == 0); // No elements yet, so 1st element is index 0.
+
+    SM.insert("gsd");
+    SM.insert("Whisperity");
+    SM.insert("John");
+    SM.insert("Hyrum");
+
+    assert(SM.size() == 4 && SM.capacity() == 4);
+    assert(SM.next_index() == 4); // [0, 1, 2, 3] are allocated indices.
+    assert(SM.is_contiguous());
+
+    SM.erase("gsd");
+
+    assert(SM.size() == 3 && SM.capacity() == 4 && SM.next_index() == 0);
+    assert(!SM.is_contiguous());
+
+    try
+    {
+        SM["gsd"];
+        assert(false && "Unreachable.");
+    } catch (const std::domain_error&) {}
+
+    SM.delete_all([](auto&& E) -> bool
+                  {
+                      assert(E != "gsd" && "Deleted element remained in iteration?");
+                      return false; // Don't delete anything!
+                  });
+
+    SM.erase("Bryce"); // Deleting non-existent element.
+    assert(!SM.is_contiguous() && SM.size() == 3 && SM.capacity() == 4 &&
+           SM.next_index() == 0);
+
+    SM.erase("John");
+    assert(SM.next_index() == 0); // [-, 1, -, 3] are allocated indices.
+    assert(SM.size() == 2 && SM.capacity() == 4);
+    assert(!SM.is_contiguous());
+
+    try
+    {
+        SM[2];
+        assert(false && "Unreachable.");
+    } catch (const std::out_of_range&) {}
+
+    auto IR1 = SM.insert("Hyrum");
+    assert(IR1.second == false && IR1.first->first == 3);
+
+    auto IR2 = SM.insert("Bjarne");
+    assert(IR2.second == true &&
+           IR2.first->first == 0); // Insert at first "good" hole.
+    assert(SM.size() == 3 && SM.capacity() == 4);
+    assert(!SM.is_contiguous());
+
+    assert(SM.next_index() == 2);
+    auto IR3 = SM.insert("Herb");
+    assert(IR3.second == true &&
+           IR3.first->first == 2); // Insert at first "good" hole.
+    assert(SM.is_contiguous());
+    assert(SM.capacity() == 4);
+
+    SM.insert("Alexandrescu");
+    assert(SM.size() == 5 && SM.next_index() == 5 && SM.capacity() == 5);
+    assert(SM.is_contiguous());
+
+    id_bimap<SMFCounter> SMFM;
+    SMFM.emplace(1);
+    SMFM.emplace(2);
+
+    SMFCounter::reset();
+    SMFM.erase(0);
+    assert(SMFM.size() == 1 && SMFM.capacity() == 2);
+
+    assert(SMFCounter::Ctor == 0 && SMFCounter::CCtor == 0 &&
+           SMFCounter::MCtor == 0 && SMFCounter::CAsg == 0 &&
+           SMFCounter::MAsg == 0 && SMFCounter::Dtor == 1 &&
+           "erase() should destroy removed elements!");
+}
+
+#include <cassert>
+#include <stdexcept>
+
+void F3b_reserve()
+{
+    string_id_bimap SM;
+    SM.reserve(4);
+
+    assert(SM.size() == 0 && SM.capacity() == 4);
+
+    SM.insert("gsd");        // 0
+    SM.insert("Whisperity"); // 1
+    SM.insert("Herb");       // 2
+    SM.insert("Xazax");      // 3
+    SM.insert("Bryce");      // 4
+
+    assert(SM.size() == 5 && SM.capacity() == 5);
+
+    SM.erase("Herb");
+    assert(SM.size() == 4 && SM.capacity() == 5);
+    try
+    {
+        SM[2];
+        assert(false && "Unreachable.");
+    } catch (const std::out_of_range&) {}
+
+    auto S1 = SM[0];
+    auto S2 = SM[3];
+
+    // Valid: [0, 1, -, 3, 4]
+
+    SM.reserve(3); // Nothing happens, as there would be elements AFTER the
+    // potentially shrinked size!
+    assert(SM.size() == 4 && SM.capacity() == 5);
+    // Valid: [0, 1, -, 3, 4]
+
+    SM.reserve(8);
+    assert(SM.size() == 4 && SM.capacity() == 8);
+    // Valid: [0, 1, -, 3, 4, -, -, -]
+
+    SM.reserve(5);
+    int i = 0;
+    // Valid: [0, 1, -, 3, 4]
+    assert(SM.size() == 4 && SM.capacity() == 5);
+
+    SMFCounter::reset();
+    id_bimap<SMFCounter> SMFM;
+    SMFM.reserve(1024);
+    assert(SMFM.size() == 0 && SMFM.capacity() == 1024);
+    assert(SMFCounter::Ctor == 0 && SMFCounter::CCtor == 0 &&
+           SMFCounter::MCtor == 0 && SMFCounter::CAsg == 0 &&
+           SMFCounter::MAsg == 0 && SMFCounter::Dtor == 0 &&
+           "reserve() should not directly construct any elements!");
+}
 
 int main() {
     //F0_types();
     //F1_operations();
-    F2_advanced();
+    //F2_advanced();
+    F3_logicalDelete();
+    F3b_reserve();
 }
